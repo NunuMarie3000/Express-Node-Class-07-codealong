a03b147cac035717595d0ec2e7250da40c330640
Storm OBryant <vmarie1997@gmail.com>
2022-08-22 14:21:39 -0500

    end of codealong

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ require('dotenv').config();
 //bring in express module
 const express = require('express');
 
-//
+const starWars = {
+    characterName: 'Princess Leia',
+    age: 'older',
+    lightsaberColor: 'not applicable',
+    father: 'Darth Vader ?',
+    quip: 'Being a baddie'
+}
 
 //bring in the port from env
 const port = process.env.PORT || 3002;
@@ -17,19 +23,25 @@ const app = express();
 //routes are endpoints, lets make one
 app.get('/', (req, res)=>{
     res.send('Hello World!!!');
-})
+});
 
 //send user a star wars character
 app.get('/starwars', (req, res)=>{
-    res.send({
-        characterName: 'Princess Leia',
-        age: 'older',
-        lightsaberColor: 'not applicable',
-        father: 'Darth Vader ?',
-        quip: 'Being a baddie'
-    });
-})
+    res.send(starWars);
+});
 
+app.get('/starwars/:id', (req, res)=>{
+    // console.log(req.params.id);
+    // res.send(req.params.id);
+    if(req.params.id !== 'leia'){
+        console.log('not leia');
+        res.sendStatus(400)
+    }else{
+        console.log('leia!!!')
+        // res.sendStatus(200)
+        res.send(starWars)
+    }
+})
 
 
 //listen for the port/turns on the express server
